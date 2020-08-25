483f3ac9c29579c3124a2127ad0f34cf732e4598
dengbohhxx <dengbohhxx@sina.com>
2020-08-25 23:50:51 +0800

    delete get_logger and timmer

--- train.py
+++ train.py
@@ -40,45 +40,7 @@ def set_seed(seed: int = 42):
     torch.cuda.manual_seed(seed)  # type: ignore
     torch.backends.cudnn.deterministic = True  # type: ignore
     torch.backends.cudnn.benchmark = True  # type: ignore
-    
-###打印日志###
-def get_logger(out_file=None):
-    logger = logging.getLogger()
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-    logger.handlers = []
-    logger.setLevel(logging.INFO)
-
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    handler.setLevel(logging.INFO)
-    logger.addHandler(handler)
-
-    if out_file is not None:
-        fh = logging.FileHandler(out_file)
-        fh.setFormatter(formatter)
-        fh.setLevel(logging.INFO)
-        logger.addHandler(fh)
-    logger.info("logger set up")
-    return logger
-    
-###时间### 
-@contextmanager
-def timer(name: str, logger: Optional[logging.Logger] = None):
-    t0 = time.time()
-    msg = f"[{name}] start"
-    if logger is None:
-        print(msg)
-    else:
-        logger.info(msg)
-    yield
-
-    msg = f"[{name}] done in {time.time() - t0:.2f} s"
-    if logger is None:
-        print(msg)
-    else:
-        logger.info(msg)
-    
-    
+       
 set_seed(1024)
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
