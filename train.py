ab40c22bc3f3e32c71e19885ce5efb4b9942c414
dengbohhxx <dengbohhxx@sina.com>
2020-08-27 06:53:48 +0800

    finish train.py

--- train.py
+++ train.py
@@ -3,33 +3,26 @@
 
 import cv2
 import audioread
-import logging
+
 import os
 import random
-import time
-import warnings
 
 import librosa
 import librosa.display as display
 import numpy as np
 import pandas as pd
-import soundfile as sf
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torch.utils.data as data
+from torch.utils.data import RandomSampler,SequentialSampler
 
-from contextlib import contextmanager
-from IPython.display import Audio
 from pathlib import Path
-from typing import Optional, List
-
-from catalyst.dl import SupervisedRunner, State, CallbackOrder, Callback, CheckpointCallback
-from fastprogress import progress_bar
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import f1_score, average_precision_score
 
+from dataset.mp3_dataset import Ori_Mp3_Dataset
+from utlis.fitter import Fitter
+from models.backbones import  Cnn14_16k 
+from config.config_Cnn14_16k import model_config,TrainGlobalConfig
+from models.Trainer import trainer
+from models.Loss import PANNsLoss
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ###设定种子###
 def set_seed(seed: int = 42):
@@ -39,137 +32,77 @@ def set_seed(seed: int = 42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)  # type: ignore
     torch.backends.cudnn.deterministic = True  # type: ignore
-    torch.backends.cudnn.benchmark = True  # type: ignore
-       
+    torch.backends.cudnn.benchmark = True  # type: ignore       
 set_seed(1024)
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
 ###路径设置###
 ROOT = Path.cwd()
 print(ROOT)
 RAW_DATA = ROOT / "birdsong-recognition"
 TRAIN_AUDIO_DIR = RAW_DATA / "train_audio"
-TEST_AUDIO_DIR = RAW_DATA / "example_test_audio"
-
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-###导入数据###
-train = pd.read_csv(RAW_DATA/"train.csv")
-test = pd.read_csv(RAW_DATA/"test.csv")
-
-
-
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ###k折交叉验证###
-
-
-
-
-
-
-
-
-
-
+train_data = pd.read_csv(RAW_DATA/"train.csv")
+tmp_list = []
+for ebird_d in TRAIN_AUDIO_DIR.iterdir():
+    for mp3_file in ebird_d.iterdir():
+        tmp_list.append([ebird_d.name, mp3_file.name, mp3_file.as_posix()])
+train_mp3_path_exist = pd.DataFrame(tmp_list, columns=["ebird_code", "filename", "file_path"])
+del tmp_list
+train_all = pd.merge(
+    train_data, train_mp3_path_exist, on=["ebird_code", "filename"], how="inner")
+train_all.loc[:, 'fold'] = 0
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=1024)
+for fold_number, (train_index, val_index) in enumerate(skf.split(train_all, train_all["ebird_code"])):
+    train_all.loc[train_all.iloc[val_index].index, 'fold'] = fold_number
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-###dateset###
-BIRD_CODE = {
-    'aldfly': 0, 'ameavo': 1, 'amebit': 2, 'amecro': 3, 'amegfi': 4,
-    'amekes': 5, 'amepip': 6, 'amered': 7, 'amerob': 8, 'amewig': 9,
-    'amewoo': 10, 'amtspa': 11, 'annhum': 12, 'astfly': 13, 'baisan': 14,
-    'baleag': 15, 'balori': 16, 'banswa': 17, 'barswa': 18, 'bawwar': 19,
-    'belkin1': 20, 'belspa2': 21, 'bewwre': 22, 'bkbcuc': 23, 'bkbmag1': 24,
-    'bkbwar': 25, 'bkcchi': 26, 'bkchum': 27, 'bkhgro': 28, 'bkpwar': 29,
-    'bktspa': 30, 'blkpho': 31, 'blugrb1': 32, 'blujay': 33, 'bnhcow': 34,
-    'boboli': 35, 'bongul': 36, 'brdowl': 37, 'brebla': 38, 'brespa': 39,
-    'brncre': 40, 'brnthr': 41, 'brthum': 42, 'brwhaw': 43, 'btbwar': 44,
-    'btnwar': 45, 'btywar': 46, 'buffle': 47, 'buggna': 48, 'buhvir': 49,
-    'bulori': 50, 'bushti': 51, 'buwtea': 52, 'buwwar': 53, 'cacwre': 54,
-    'calgul': 55, 'calqua': 56, 'camwar': 57, 'cangoo': 58, 'canwar': 59,
-    'canwre': 60, 'carwre': 61, 'casfin': 62, 'caster1': 63, 'casvir': 64,
-    'cedwax': 65, 'chispa': 66, 'chiswi': 67, 'chswar': 68, 'chukar': 69,
-    'clanut': 70, 'cliswa': 71, 'comgol': 72, 'comgra': 73, 'comloo': 74,
-    'commer': 75, 'comnig': 76, 'comrav': 77, 'comred': 78, 'comter': 79,
-    'comyel': 80, 'coohaw': 81, 'coshum': 82, 'cowscj1': 83, 'daejun': 84,
-    'doccor': 85, 'dowwoo': 86, 'dusfly': 87, 'eargre': 88, 'easblu': 89,
-    'easkin': 90, 'easmea': 91, 'easpho': 92, 'eastow': 93, 'eawpew': 94,
-    'eucdov': 95, 'eursta': 96, 'evegro': 97, 'fiespa': 98, 'fiscro': 99,
-    'foxspa': 100, 'gadwal': 101, 'gcrfin': 102, 'gnttow': 103, 'gnwtea': 104,
-    'gockin': 105, 'gocspa': 106, 'goleag': 107, 'grbher3': 108, 'grcfly': 109,
-    'greegr': 110, 'greroa': 111, 'greyel': 112, 'grhowl': 113, 'grnher': 114,
-    'grtgra': 115, 'grycat': 116, 'gryfly': 117, 'haiwoo': 118, 'hamfly': 119,
-    'hergul': 120, 'herthr': 121, 'hoomer': 122, 'hoowar': 123, 'horgre': 124,
-    'horlar': 125, 'houfin': 126, 'houspa': 127, 'houwre': 128, 'indbun': 129,
-    'juntit1': 130, 'killde': 131, 'labwoo': 132, 'larspa': 133, 'lazbun': 134,
-    'leabit': 135, 'leafly': 136, 'leasan': 137, 'lecthr': 138, 'lesgol': 139,
-    'lesnig': 140, 'lesyel': 141, 'lewwoo': 142, 'linspa': 143, 'lobcur': 144,
-    'lobdow': 145, 'logshr': 146, 'lotduc': 147, 'louwat': 148, 'macwar': 149,
-    'magwar': 150, 'mallar3': 151, 'marwre': 152, 'merlin': 153, 'moublu': 154,
-    'mouchi': 155, 'moudov': 156, 'norcar': 157, 'norfli': 158, 'norhar2': 159,
-    'normoc': 160, 'norpar': 161, 'norpin': 162, 'norsho': 163, 'norwat': 164,
-    'nrwswa': 165, 'nutwoo': 166, 'olsfly': 167, 'orcwar': 168, 'osprey': 169,
-    'ovenbi1': 170, 'palwar': 171, 'pasfly': 172, 'pecsan': 173, 'perfal': 174,
-    'phaino': 175, 'pibgre': 176, 'pilwoo': 177, 'pingro': 178, 'pinjay': 179,
-    'pinsis': 180, 'pinwar': 181, 'plsvir': 182, 'prawar': 183, 'purfin': 184,
-    'pygnut': 185, 'rebmer': 186, 'rebnut': 187, 'rebsap': 188, 'rebwoo': 189,
-    'redcro': 190, 'redhea': 191, 'reevir1': 192, 'renpha': 193, 'reshaw': 194,
-    'rethaw': 195, 'rewbla': 196, 'ribgul': 197, 'rinduc': 198, 'robgro': 199,
-    'rocpig': 200, 'rocwre': 201, 'rthhum': 202, 'ruckin': 203, 'rudduc': 204,
-    'rufgro': 205, 'rufhum': 206, 'rusbla': 207, 'sagspa1': 208, 'sagthr': 209,
-    'savspa': 210, 'saypho': 211, 'scatan': 212, 'scoori': 213, 'semplo': 214,
-    'semsan': 215, 'sheowl': 216, 'shshaw': 217, 'snobun': 218, 'snogoo': 219,
-    'solsan': 220, 'sonspa': 221, 'sora': 222, 'sposan': 223, 'spotow': 224,
-    'stejay': 225, 'swahaw': 226, 'swaspa': 227, 'swathr': 228, 'treswa': 229,
-    'truswa': 230, 'tuftit': 231, 'tunswa': 232, 'veery': 233, 'vesspa': 234,
-    'vigswa': 235, 'warvir': 236, 'wesblu': 237, 'wesgre': 238, 'weskin': 239,
-    'wesmea': 240, 'wessan': 241, 'westan': 242, 'wewpew': 243, 'whbnut': 244,
-    'whcspa': 245, 'whfibi': 246, 'whtspa': 247, 'whtswi': 248, 'wilfly': 249,
-    'wilsni1': 250, 'wiltur': 251, 'winwre3': 252, 'wlswar': 253, 'wooduc': 254,
-    'wooscj2': 255, 'woothr': 256, 'y00475': 257, 'yebfly': 258, 'yebsap': 259,
-    'yehbla': 260, 'yelwar': 261, 'yerwar': 262, 'yetvir': 263
-}
-
-INV_BIRD_CODE = {v: k for k, v in BIRD_CODE.items()}
-PERIOD = 5
-
-class PANNsDataset(data.Dataset):
-    def __init__(self,file_list: List[List[str]],waveform_transforms=None):
-        self.file_list = file_list  # list of list: [file_path, ebird_code]
-        self.waveform_transforms = waveform_transforms
-
-    def __len__(self):
-        return len(self.file_list)
-
-    def __getitem__(self, idx: int):
-        wav_path, ebird_code = self.file_list[idx]
-
-        y, sr = sf.read(wav_path)
-
-        if self.waveform_transforms:
-            y = self.waveform_transforms(y)
-        else:
-            len_y = len(y)
-            effective_length = sr * PERIOD
-            if len_y < effective_length:
-                new_y = np.zeros(effective_length, dtype=y.dtype)
-                start = np.random.randint(effective_length - len_y)
-                new_y[start:start + len_y] = y
-                y = new_y.astype(np.float32)
-            elif len_y > effective_length:
-                start = np.random.randint(len_y - effective_length)
-                y = y[start:start + effective_length].astype(np.float32)
-            else:
-                y = y.astype(np.float32)
-
-        labels = np.zeros(len(BIRD_CODE), dtype="f")
-        labels[BIRD_CODE[ebird_code]] = 1
-
-        return {"waveform": y, "targets": labels}
-
-
-
+###构建模型###
+model=Cnn14_16k(**model_config)
+checkpoint=torch.load("./pretrained_weight/Cnn14_16k_mAP=0.438.pth",map_location="cpu")
+pretrained_weights=checkpoint["model"]
+model_dict=model.state_dict()
+new_dict = {k: v for k, v in pretrained_weights.items() if k.find("fc_audioset")==-1 and k in model_dict.keys()}
+model_dict.update(new_dict)
+model.load_state_dict(model_dict)
+net=trainer(model,PANNsLoss())   
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-###dataloader###
+def collate_fn(batch):
+    return tuple(zip(*batch))
+device = torch.device('cpu')
+net.to(device)
+fitter = Fitter(model=net, device=device, config=TrainGlobalConfig)
+for i in range(TrainGlobalConfig.n_epochs):    
+    fold_number = i%5
+    train_dataset = Ori_Mp3_Dataset(
+        sounds_id=train_all[train_all['fold'] != fold_number].index.values,
+        train_all=train_all,
+        waveform_transforms=None,
+    )
 
+    validation_dataset = Ori_Mp3_Dataset(
+        sounds_id=train_all[train_all['fold'] == fold_number].index.values,
+        train_all=train_all,
+        waveform_transforms=None,
+    )
+    
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=TrainGlobalConfig.batch_size,
+        sampler=RandomSampler(train_dataset),
+        pin_memory=False,
+        drop_last=True,
+        num_workers=TrainGlobalConfig.num_workers,
+        collate_fn=collate_fn,
+    )
+    val_loader = torch.utils.data.DataLoader(
+        validation_dataset,
+        batch_size=TrainGlobalConfig.batch_size,
+        num_workers=TrainGlobalConfig.num_workers,
+        shuffle=False,
+        sampler=SequentialSampler(validation_dataset),
+        pin_memory=False,
+        collate_fn=collate_fn,
+    )
+    fitter.fit(train_loader, val_loader)
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
