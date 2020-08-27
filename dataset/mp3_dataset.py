24e77d754646679137aff6cc8b17a5c6e02bb30d
dengbohhxx <dengbohhxx@sina.com>
2020-08-27 19:37:57 +0800

    change SR

--- dataset/mp3_dataset.py
+++ dataset/mp3_dataset.py
@@ -1,8 +1,8 @@
 import torch.utils.data as data
-
 import numpy as np
 import pandas as pd
 import librosa
+from config.config_Cnn14_16k import model_config 
 
 data_csv = pd.read_csv("birdsong-recognition/train.csv")
 bird_species=np.unique(np.array(list(data_csv["ebird_code"])))
@@ -20,7 +20,7 @@ class Ori_Mp3_Dataset(data.Dataset):
     def __getitem__(self, idx):
         mp3_path=self.train_csv.loc[[self.sounds_id[idx]],["file_path"]].values[0][0]
         ebird_code=self.train_csv.loc[[self.sounds_id[idx]],["ebird_code"]].values[0][0]        
-        y, sr = librosa.load(mp3_path,sr=16000, mono=True, res_type="kaiser_fast")
+        y, sr = librosa.load(mp3_path,sr=model_config["sample_rate"], mono=True, res_type="kaiser_fast")
         if self.waveform_transforms:
             y = self.waveform_transforms(y)
         else:
