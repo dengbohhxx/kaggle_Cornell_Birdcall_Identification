be98a0dc34fb4f81b1308c5fc80d4ac9b096882c
dengbohhxx <dengbohhxx@sina.com>
2020-08-29 06:34:49 +0800

    add label_smoothing

--- models/Loss.py
+++ models/Loss.py
@@ -5,11 +5,18 @@
 import torch
 import torch.nn as nn
 class PANNsLoss(nn.Module):
-    def __init__(self):
+    def __init__(self,label_smoothing=None,epsilon=0.05):
         super().__init__()
 
         self.bce = nn.BCELoss()
-
+        self.label_smoothing=label_smoothing
+        self.epsilon=epsilon
+    def label_smooth(self,label):
+        label=label+self.epsilon
+        _,index=torch.max(label,-1)  
+        for i in range(label.size()[0]):
+            label[i][index[i]]=1-self.epsilon
+        return label    
     def forward(self, input, target):
         input_ = input["clipwise_output"]
         input_ = torch.where(torch.isnan(input_),
@@ -20,5 +27,8 @@ class PANNsLoss(nn.Module):
                              input_)
 
         target = target.float()
-
+        if self.label_smoothing!=None:
+            target=self.label_smooth(target)
         return self.bce(input_, target)
+     
+        
